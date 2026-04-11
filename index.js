f875bb6a076d11ccb3e937535e0ef543cf4f28e4
promoteme-click <promoteme.click@gmail.com>
2026-04-10 21:15:56 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let express=require("express");
+let app=express();
+let cors=require("cors");
+// MiddleWares
+app.use(cors());
+app.use(express.json())
+
+let PORT=process.env.PORT|3000;
+app.listen(PORT,()=>{
+    console.log("server is running");
+})
